4fac5d8322d98676283859525af399736227f96c
Artem <a4b11s@gmail.com>
2024-10-10 23:41:15 +0300

    Refactor replay buffer implementation and add support for device selection

---
+++ qlor/replay_buffer.py
@@ -0,0 +1,45 @@
+from collections import deque
+import numpy as np
+import torch
+
+
+class ReplayBuffer:
+    def __init__(self, max_size, device):
+        self.max_size = max_size
+        self.buffer = deque(maxlen=max_size)
+
+        self.device = device
+
+    def add(self, state, action, reward, next_state):
+        state = self._add_prepare(state)
+        action = self._add_prepare(action)
+        reward = self._add_prepare(reward)
+        next_state = self._add_prepare(next_state)
+
+        self.buffer.append((state, action, reward, next_state))
+
+    def sample(self, batch_size, device=None):
+        _device = device
+
+        if _device is None:
+            _device = self.device
+
+        if _device is None:
+            _device = torch.device("cpu")
+
+        idx = np.random.choice(
+            np.arange(len(self.buffer)), size=batch_size, replace=False
+        )
+
+        return [torch.tensor(self.buffer[i], device=_device) for i in idx]
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def _add_prepare(self, data):
+        if isinstance(data, torch.Tensor):
+            return data.cpu().numpy()
+        return data
+
+    def __getitem__(self, idx):
+        return self.buffer[idx]
