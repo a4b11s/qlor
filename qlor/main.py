8610581d5f4a6903445f46ca4e46f6c62d4752e3
Artem <a4b11s@gmail.com>
2024-10-13 22:14:20 +0300

    Refactor main.py for improved code organization and training loop

--- qlor/main.py
+++ qlor/main.py
@@ -9,32 +9,34 @@ import torch
 from qlor.epsilon import Epsilon
 from qlor.trainer import Trainer
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-torch.set_default_device(device)
-
-envs = gymnasium.make_vec(
-    "VizdoomCorridor-v0",  # or any other environment id
-    num_envs=16,
-    # render_mode="human",
-)  # or any other environment id
-
-screen_shape = envs.single_observation_space["screen"].shape
-screen_shape = (screen_shape[2], screen_shape[0], screen_shape[1])
-action_dim = envs.single_action_space.n
-
-agent = Agent(screen_shape, action_dim)
-target_agent = Agent(screen_shape, action_dim).to(device)
-target_agent.load_state_dict(agent.state_dict())
-
-optimizer = optim.Adam(agent.parameters(), lr=1e-3)
-criterion = nn.MSELoss()  # Define the loss criterion
-
 
 def train():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.set_default_device(device)
+    
+    print(f"Using device: {torch.get_default_device()}")
+
+    envs = gymnasium.make_vec(
+        "VizdoomCorridor-v0",  # or any other environment id
+        num_envs=16,
+        # render_mode="human",
+    )
+
+    screen_shape = envs.single_observation_space["screen"].shape
+    screen_shape = (screen_shape[2], screen_shape[0], screen_shape[1])
+    action_dim = envs.single_action_space.n
+
+    agent = Agent(screen_shape, action_dim)
+    target_agent = Agent(screen_shape, action_dim).to(device)
+    target_agent.load_state_dict(agent.state_dict())
+
+    optimizer = optim.Adam(agent.parameters(), lr=1e-3)
+    criterion = nn.MSELoss()
+
     epsilon = Epsilon(
         start=1.0,
         end=0.01,
-        decay=0.999,
+        decay=5000,
     )
 
     trainer = Trainer(
@@ -51,6 +53,4 @@ def train():
 
 
 if __name__ == "__main__":
-
-    print(f"Using device: {torch.get_default_device()}")
     train()
